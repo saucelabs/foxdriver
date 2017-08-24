48a0e4c1a29eee9fb5845982cf260ef8626c3163
christian-bromann <mail@christian-bromann.com>
2017-08-24 13:33:14 +0200

    add first unit tests

---
+++ test/utils.test.js
@@ -0,0 +1,32 @@
+import { spy } from 'sinon'
+
+import {
+    transformEvaluateArgs, transformEvaluateScript, addEventListener, removeEventListeners
+} from '../lib/utils'
+
+test('transform script properly', () => {
+    expect(transformEvaluateScript('foo', 'someargs')).toEqual(
+        '(function () { foo }).apply(window, someargs)')
+})
+
+test('transform arguments properly', () => {
+    const args = transformEvaluateArgs([() => 'foobar', 1, '2', false, [1,2,3], {foo: 'bar'}])
+    expect(args).toEqual('["() => \'foobar\'",1,"2",false,[1,2,3],{"foo":"bar"}]')
+})
+
+test('creates enhanced listener objects', () => {
+    const emitter = { on: spy() }
+    const handler = spy()
+    addEventListener(emitter, 'foobar', handler)
+    expect(emitter.on.calledWith('foobar', handler)).toBe(true)
+})
+
+test('unregisteres listener events', () => {
+    const emitter = { removeListener: spy(), on: () => {} }
+    const handler = spy()
+    const listeners = [addEventListener(emitter, 'foobar', handler)]
+
+    removeEventListeners(listeners)
+    expect(emitter.removeListener.calledWith('foobar', handler)).toBe(true);
+    expect(listeners).toHaveLength(0)
+})
