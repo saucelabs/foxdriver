f405dc065e696fd5a055da7e27d96b817647760b
christian-bromann <mail@christian-bromann.com>
2017-09-02 18:50:32 +0200

    add unit tests for css prop actor

---
+++ test/cssProperties.test.js
@@ -0,0 +1,18 @@
+import { stub } from 'sinon'
+
+import CSSProperties from '../lib/domains/cssProperties'
+
+jest.mock('../lib/logger', () => jest.fn(() => ({ info: () => {} })))
+
+const client = { makeRequest: stub(), on: () => {} }
+client.makeRequest.returns({})
+const cssPropertiesActor = new CSSProperties(client, 'server1.conn1.child6/cssPropertiesActor1', {
+    actor: 'server1.conn1.child6/cssPropertiesActor1'
+})
+
+test('can unregister actor', () => {
+    cssPropertiesActor.getCSSDatabase()
+    expect(client.makeRequest.calledWith(
+        {to: 'server1.conn1.child6/cssPropertiesActor1', type: 'getCSSDatabase'}
+    )).toEqual(true)
+})
