762f9a42ccd574d5c61aa91f41ad7964e9900d0e
christian-bromann <mail@christian-bromann.com>
2017-09-03 17:38:35 +0200

    add unit tests for HeapSnapshotFile actor

---
+++ test/heapSnapshotFile.test.js
@@ -0,0 +1,18 @@
+import { stub } from 'sinon'
+
+import HeapSnapshotFile from '../lib/domains/heapSnapshotFile'
+
+jest.mock('../lib/logger', () => jest.fn(() => ({ info: () => {} })))
+
+const client = { makeRequest: stub(), on: () => {} }
+client.makeRequest.returns({})
+const heapSnapshotFile = new HeapSnapshotFile(client, 'server1.conn1.child6/heapSnapshotFile', {
+    actor: 'server1.conn1.child6/heapSnapshotFile'
+})
+
+test('can unregister actor', () => {
+    heapSnapshotFile.getHeapSnapshot(123)
+    expect(client.makeRequest.calledWith(
+        {to: 'server1.conn1.child6/heapSnapshotFile', type: 'transferHeapSnapshot', snapshotId: 123}
+    )).toEqual(true)
+})
