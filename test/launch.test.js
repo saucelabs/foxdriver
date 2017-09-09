fcb09d8671c7cc3d991c7ff19e951feb107393a1
christian-bromann <mail@christian-bromann.com>
2017-09-09 16:17:02 +0200

    increase integration test timeout for CI

--- test/launch.test.js
+++ test/launch.test.js
@@ -1,5 +1,7 @@
 import Foxdriver from '../lib'
 
+jest.setTimeout(30000)
+
 test('should be able to launch and attach to Firefox', async () => {
     const { browser, tab } = await Foxdriver.launch({
         url: 'https://www.mozilla.org/en-US'
